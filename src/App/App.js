e68dd4c679fe943ca1bbb4dac632a5c98308bcaf
mazouz abdelhakim <maz.hakim93@gmail.com>
2021-05-01 01:57:38 +0100

    fixing final header

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,7 @@ import React,{Component} from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import Header from './component/Header/Header';
-
+import Header from './layout/Header/Header';
 class App extends Component {
   render() {
     const store=configureStore();
--- src/App/layout/Header/Header.js
+++ src/App/layout/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Header.css';
-import HeaderLogo from '../HeaderLogo/HeaderLogo';
-import InputSearch from '../InputSearch/InputSearch';
+import HeaderLogo from '../../component/HeaderLogo/HeaderLogo';
+import InputSearch from '../../component/InputSearch/InputSearch';
 import { RiShoppingCartLine } from 'react-icons/ri';
 import { IconContext } from "react-icons";
-import HeaderItem from '../HeaderItem/HeaderItem';
+import HeaderItem from '../../component/HeaderItem/HeaderItem';
 function Header(props) {
     return (
         <div className='container-header' >
@@ -15,7 +15,7 @@ function Header(props) {
                 <div className='header-right'>
                     <HeaderItem title='FAQ'/>
                     <HeaderItem title='Track order'/>
-                    <IconContext.Provider value={{  color:'#fff',size:70}}> 
+                    <IconContext.Provider value={{  color:'dark',size:50}}> 
 
                     <RiShoppingCartLine />
 
