edaba3e32d80e51e0aad89df57a8ea93e4e0f740
mazouz abdelhakim <maz.hakim93@gmail.com>
2021-05-07 15:58:24 +0100

    adding book details

--- src/App/component/DetailBook/DetailBook.js
+++ src/App/component/DetailBook/DetailBook.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import './DetailBook.css'
+import './DetailBook.css';
+import bookImage from '../../../assets/learn-react.png'
+import { IconContext } from "react-icons";
+import {AiFillStar} from 'react-icons/ai'
 function DetailBook(props) {
     return (
         <div className='detail-book'>
-            
+            <img className='detail-book-image' src={bookImage}/>
+            <div className='detail-book-information-section'>
+                 <p>the is on fire</p>
+                 <p> albert chrisotphe</p>
+                 <p> $50.00</p>
+                 {Array(5)
+                            .fill()
+                            .map((_, i) => (
+                               
+                                <IconContext.Provider value={{  color:'#FF5700',size:20}}> 
+                                    <AiFillStar  key={i}/>
+                                </IconContext.Provider>
+                            ))}
+                <button >hello</button>
+            </div>
         </div>
     );
 }
